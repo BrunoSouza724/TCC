789fccc2e29b157c71aee12bfddfafe69f99390f
Bruno Fernandes de Souza <111061043+BrunoSouza724@users.noreply.github.com>
2024-12-01 13:25:07 -0300

    Update and rename Painel Gerencial to app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -3,9 +3,6 @@ import streamlit as st
 st.title('Seja-Bem Vindo ao Controle Financeiro :heavy_dollar_sign:')
 
 st.sidebar.title('Opções')
-
 st.sidebar.header(' :credit_card: Entrada e Saída')
 st.sidebar.header('	:chart: Demonstrações Financeiras')
 st.sidebar.header(' :moneybag: Resumo Financeiro')
-
-
