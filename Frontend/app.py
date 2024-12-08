d062a32dffb85d05c598999d8b105cc97c88203f
Bruno Fernandes de Souza <111061043+BrunoSouza724@users.noreply.github.com>
2024-12-08 09:46:26 -0300

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
-st.title('Seja-Bem Vindo ao Controle Financeiro :heavy_dollar_sign:')
+def main():
+    st.title('Seja-Bem Vindo ao Controle Financeiro :heavy_dollar_sign:')
 
-st.sidebar.title('Opções')
-st.sidebar.header(' :credit_card: Entrada e Saída')
-st.sidebar.header('	:chart: Demonstrações Financeiras')
-st.sidebar.header(' :moneybag: Resumo Financeiro')
+    st.sidebar.title('Opções')
+    st.sidebar.header(':credit_card: Entrada e Saída')
+    st.sidebar.header(':chart: Demonstrações Financeiras')
+    st.sidebar.header(':moneybag: Resumo Financeiro')
+
+if __name__ == "__main__":
+    main()
